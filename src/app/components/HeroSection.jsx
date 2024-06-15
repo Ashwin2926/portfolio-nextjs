79c925cce4e3e6278fddd01e340ca3b68a36ad3f
Ashwin Nyamainashe <104971785+Ashwin2926@users.noreply.github.com>
2024-06-15 22:59:28 +0400

    commit media query

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,8 +15,8 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-black font-bold mb-4 text-4xl sm:text-5xl lg:leading-normal  ">
-            <span className=" reduce text-transparent font-bold bg-clip-text bg-gradient-to-br from-blue-500 to-blue-700">
+          <p className="text-black font-bold mb-4 text-4xl sm:text-5xl lg:leading-normal  ">
+            <span className=" text-transparent font-bold bg-clip-text bg-gradient-to-br from-blue-500 to-blue-700">
               Hi, I&apos;m Ashwin {" "}
             </span>
             <br></br>
@@ -37,7 +37,7 @@ const HeroSection = () => {
               speed={50}
               repeat={Infinity}
             />
-          </h1>
+          </p>
           <p className="text-[#000000] text-base sm:text-lg mb-6 lg:text-xl">
           Delivering Innovative Software Solutions with Precision.
           </p>
